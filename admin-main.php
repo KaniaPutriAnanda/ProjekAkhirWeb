96a84aef7e8017505ad66047453880821c85588c
NurmedinaMaulidiah <93064939+NurmedinaMaulidiah@users.noreply.github.com>
2022-11-02 22:58:04 +0800

    Add files via upload

---
+++ admin-main.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin</title>
+    <style><?php include 'css/style-admin.css' ?></style>
+    <link rel = "stylesheet" href = "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" />
+</head>
+<body>
+    <nav>
+        <ul>
+            <li>
+                <a href="#" class="logo">
+                <i class="fas fa-search"></i>
+                    <span class="nav-item">Lihat  Menu</span>
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                <i class="fas fa-plus"></i>
+                    <span class="nav-item">Tambah Menu</span>
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                <i class="fas fa-edit"></i>
+                    <span class="nav-item">Update Menu</span>
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                <i class="fas fa-trash"></i>
+                    <span class="nav-item">Hapus Menu</span>
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                <i class="fas fa-cash-register"></i>
+                    <span class="nav-item">Kasir</span>
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                <i class="fas fa-sign-out-alt"></i>
+                    <span class="nav-item">Signout</span>
+                </a>
+            </li>
+        </ul>
+    </nav>
+</body>
+</html>
