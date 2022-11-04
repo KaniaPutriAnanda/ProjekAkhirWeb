337b47b3d6995edc6aceb813643cc2418a25099f
NurmedinaMaulidiah <93064939+NurmedinaMaulidiah@users.noreply.github.com>
2022-11-04 11:42:57 +0800

    Update and rename admin-main.php to index-admin.php
    ubah nama file jdi index-admin

--- index-admin.php
+++ index-admin.php
@@ -3,16 +3,17 @@
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <link rel="stylesheet" href="css/admin3.css">
+    <style><?php include 'admin.css' ?></style>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Admin</title>
-    <style><?php include 'css/style-admin.css' ?></style>
     <link rel = "stylesheet" href = "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" />
+    <title>Document</title>
 </head>
 <body>
-    <nav>
+<nav>
         <ul>
             <li>
-                <a href="#" class="logo">
+                <a href="lihat.php" class="logo">
                 <i class="fas fa-search"></i>
                     <span class="nav-item">Lihat  Menu</span>
                 </a>
@@ -21,16 +22,22 @@
                 <a href="#">
                 <i class="fas fa-plus"></i>
                     <span class="nav-item">Tambah Menu</span>
+                    <span class="fas fa-caret-down"  id="panah"></span>
                 </a>
+                <ul class="feat-show">
+                    <li><a href="makanan.php">Makanan</a></li>
+                    <li><a href="minuman.php">Minuman</a></li>
+                    <li><a href="dessert.php">Dessert</a></li>
+                </ul>
             </li>
             <li>
-                <a href="#">
+                <a href="lihat-edit.php">
                 <i class="fas fa-edit"></i>
                     <span class="nav-item">Update Menu</span>
                 </a>
             </li>
             <li>
-                <a href="#">
+                <a href="lihat-hapus.php">
                 <i class="fas fa-trash"></i>
                     <span class="nav-item">Hapus Menu</span>
                 </a>
@@ -49,5 +56,3 @@
             </li>
         </ul>
     </nav>
-</body>
-</html>
