456918a57129b865355a7c23e4c0d0262467d686
KaniaPutriAnanda <93080099+KaniaPutriAnanda@users.noreply.github.com>
2022-11-04 15:07:17 +0800

    Update lihat.php
    di edit bagian tabelnya

--- lihat.php
+++ lihat.php
@@ -7,76 +7,152 @@
     <title>Document</title>
 </head>
 <body>
-<table border=1>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin</title>
+    <link rel="stylesheet" href="admin3.css?v7">
+    <style><?php include 'admin.css' ?></style>
+    <link rel = "stylesheet" href = "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" />
+</head>
+<body>
+    <nav>
+        <ul>
+            <li>
+                <a href="#" class="logo">
+                <i class="fas fa-search"></i>
+                    <span class="nav-item">Lihat  Menu</span>
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                <i class="fas fa-plus"></i>
+                    <span class="nav-item">Tambah Menu</span>
+                    <span class="fas fa-caret-down" id="panah"></span>
+                </a>
+                <ul class="feat-show">
+                    <li><a href="makanan.php">Makanan</a></li>
+                    <li><a href="minuman.php">Minuman</a></li>
+                    <li><a href="dessert.php">Dessert</a></li>
+                </ul>
+            </li>
+            <li>
+                <a href="#">
+                <i class="fas fa-edit"></i>
+                    <span class="nav-item">Update Menu</span>
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                <i class="fas fa-trash"></i>
+                    <span class="nav-item">Hapus Menu</span>
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                <i class="fas fa-cash-register"></i>
+                    <span class="nav-item">Kasir</span>
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                <i class="fas fa-sign-out-alt"></i>
+                    <span class="nav-item">Signout</span>
+                </a>
+            </li>
+        </ul>
+    </nav>
+<div class="bigbox">
+    <div class="tabel1">
+        <table border='5'>
         <tr>
-            <th>no</th>
-            <th>nama</th>
-            <th>harga</th>
-            <th>foto</th>
+            <th colspan='4' class="head">--*MAKANAN*--</th>
         </tr>
-            <?php
-                require 'konfigurasi.php';
-                $result = $db->query("SELECT * FROM makanan");
-                $i = 1;
-                while($row = mysqli_fetch_array($result)){
-            ?>
-        <tr>
-            <td><?=$i?></td>
-            <td><?=$row['nama']?></td>
-            <td><?=$row['harga']?></td>
-            <td><img src="img/<?=$row['gambar']?>" alt="" width="100px"></td>
-        </tr>
-            <?php
-                $i++; }
-            ?>
-    </table>
+                <tr>
+                    <th>no</th>
+                    <th>Makanan</th>
+                    <th>harga</th>
+                    <th>foto</th>
+                </tr>
+                    <?php
+                        require 'konfigurasi.php';
+                        $result = $db->query("SELECT * FROM makanan");
+                        $i = 1;
+                        while($row = mysqli_fetch_array($result)){
+                    ?>
+                <tr>
+                    <td><?=$i?></td>
+                    <td><?=$row['nama']?></td>
+                    <td><?=$row['harga']?></td>
+                    <td><img src="img/<?=$row['gambar']?>" alt="" width="100px"></td>
+                </tr>
+                    <?php
+                        $i++; }
+                    ?>
+        </table>
+    </div>
 
-    <table border=1>
+    <div class="tabel2">
+        <table>
         <tr>
-            <th>no</th>
-            <th>nama</th>
-            <th>harga</th>
-            <th>foto</th>
+            <th colspan='4'class="head">--*MINUMAN*--</th>
         </tr>
-            <?php
-                require 'konfigurasi.php';
-                $result = $db->query("SELECT * FROM minuman");
-                $i = 1;
-                while($row = mysqli_fetch_array($result)){
-            ?>
-        <tr>
-            <td><?=$i?></td>
-            <td><?=$row['nama']?></td>
-            <td><?=$row['harga']?></td>
-            <td><img src="img/<?=$row['gambar']?>" alt="" width="100px"></td>
-        </tr>
-            <?php
-                $i++; }
-            ?>
-    </table>
 
-    <table border=1>
+            <tr>
+                <th>no</th>
+                <th>minuman</th>
+                <th>harga</th>
+                <th>foto</th>
+            </tr>
+                <?php
+                    require 'konfigurasi.php';
+                    $result = $db->query("SELECT * FROM minuman");
+                    $i = 1;
+                    while($row = mysqli_fetch_array($result)){
+                ?>
+            <tr>
+                <td><?=$i?></td>
+                <td><?=$row['nama']?></td>
+                <td><?=$row['harga']?></td>
+                <td><img src="img/<?=$row['gambar']?>" alt="" width="100px"></td>
+            </tr>
+                <?php
+                    $i++; }
+                ?>
+        </table>
+    </div>
+
+    <div class="tabel3">
+        <table>
         <tr>
-            <th>no</th>
-            <th>nama</th>
-            <th>harga</th>
-            <th>foto</th>
+            <th colspan='4'class="head">--*Dessert*--</th>
         </tr>
-            <?php
-                require 'konfigurasi.php';
-                $result = $db->query("SELECT * FROM dessert");
-                $i = 1;
-                while($row = mysqli_fetch_array($result)){
-            ?>
-        <tr>
-            <td><?=$i?></td>
-            <td><?=$row['nama']?></td>
-            <td><?=$row['harga']?></td>
-            <td><img src="img/<?=$row['gambar']?>" alt="" width="100px"></td>
-        </tr>
-            <?php
-                $i++; }
-            ?>
-    </table>
+            <tr>
+                <th>no</th>
+                <th>dessert</th>
+                <th>harga</th>
+                <th>foto</th>
+            </tr>
+                <?php
+                    require 'konfigurasi.php';
+                    $result = $db->query("SELECT * FROM dessert");
+                    $i = 1;
+                    while($row = mysqli_fetch_array($result)){
+                ?>
+            <tr>
+                <td><?=$i?></td>
+                <td><?=$row['nama']?></td>
+                <td><?=$row['harga']?></td>
+                <td><img src="img/<?=$row['gambar']?>" alt="" width="100px"></td>
+            </tr>
+                <?php
+                    $i++; }
+                ?>
+        </table>
+    </div>
+</div>
 </body>
 </html>
