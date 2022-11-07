32555ea9ec7e34af70533bf4f28570b2cc99c04f
KaniaPutriAnanda <93080099+KaniaPutriAnanda@users.noreply.github.com>
2022-11-08 03:09:31 +0800

    Add files via upload

---
+++ menu.php
@@ -0,0 +1,129 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style-home2.css?v3">
+    <link rel = "stylesheet" href = "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" />
+</head>
+<body>
+<nav>
+        <!-- <div class="logo">
+           <img src="img/logo.png" alt="" width="70px"> 
+        </div> -->
+
+        <ul>
+            <li> <a href="">Home</a></li>
+            <li> <a href="">Menu</a></li>
+            <li> <a href="">Reservasi</a></li>
+            <li> <a href="">About Us</a></li>
+            <li> <a href="">Sign Out</a></li>
+        </ul>
+
+        <div class="menu-toggle">
+            <input type="checkbox"/>
+            <span></span>
+            <span></span>
+            <span></span>
+        </div>
+    </nav>
+
+    <div class="bigbox">
+
+        <div class="foto"><img name="slide" width="80%"></div>
+
+        <div class="mkn">
+            <div class="mkn1"><h1>MAKANAN</h1></div>
+            <div class="mkn2"><h4>Menu Favorit Korea yang bisa temani kamu Nonton Drakor.</h4></div>
+            <div class="mknfoto">
+                <div class="mknfoto1"><a href="#"><img src="img/mkn1.jpg" alt="" width="250px"></a><p>Bossam</p><p>Rp. 150,000</p></div>
+                <div class="mknfoto1"><a href="#"><img src="img/mkn2.jpg" alt="" width="250px"></a><p>Jap-Chae</p><p>Rp. 100,000</p></div>
+                <div class="mknfoto1"><a href="#"><img src="img/mkn3.jpg" alt="" width="250px"></a><p>Spicy Rice Cake</p><p>Rp. 55,000</p></div>
+                <div class="mknfoto1"><a href="#"><img src="img/mkn4.jpg" alt="" width="250px"></a><p>Bulgogi</p><p>Rp. 175,000</p></div>
+                <div class="mknfoto1"><a href="#"><img src="img/mkn5.jpg" alt="" width="250px"></a><p>Kimchi Stew</p><p>Rp. 130,000</p></div>
+                <div class="mknfoto1"><a href="#"><img src="img/mkn6.jpg" alt="" width="250px"></a><p>Rice Cake Dumpling Soup</p><p>Rp. 80,000</p></div>
+                <div class="mknfoto1"><a href="#"><img src="img/mkn7.jpg" alt="" width="250px"></a><p>Spicy Chicken Dolsot</p><p>Rp. 100,000</p></div>
+                <div class="mknfoto1"><a href="#"><img src="img/mkn8.jpg" alt="" width="250px"></a><p>Spicy Chicken w/ Cheese</p><p>Rp. 150,000</p></div>
+                <div class="mknfoto1"><a href="#"><img src="img/mkn9.jpg" alt="" width="250px"></a><p>Tofu Teriyaki</p><p>Rp. 50,000</p></div>
+            </div>
+        </div>
+        <hr>
+
+        <div class="mnm">
+            <div class="mnm1"><h1>Minuman</h1></div>
+            <div class="mnm2"><h4>Rasakan segar, dingin, dan creamynya berbagai varian minuman favorit khas Korea.</h4></div>
+            <div class="mnmfoto">
+                <div class="mnmfoto1"><a href="#"><img src="https://i.pinimg.com/564x/0f/b6/f6/0fb6f6bfd4e395b90092777f4f81ebfc.jpg" alt="" width="250px"></a><p>Pink Flag</p><p>Rp. 70,000</p></div>
+                <div class="mnmfoto1"><a href="#"><img src="https://i.pinimg.com/564x/21/1f/39/211f39072f0962ab3937a36607df39e6.jpg" alt="" width="250px"></a><p>White Soul</p><p>Rp. 85,000</p></div>
+                <div class="mnmfoto1"><a href="#"><img src="https://i.pinimg.com/564x/f9/77/40/f977403cb5171de8587b466d1403b82f.jpg" alt="" width="250px"></a><p>Win Heart</p><p>Rp. 70,000</p></div>
+                <div class="mnmfoto1"><a href="#"><img src="https://i.pinimg.com/564x/bd/73/e0/bd73e0ff7dec5ac3eba138056d230787.jpg" alt="" width="250px"></a><p>Blue Sky</p><p>Rp. 50,000</p></div>
+                <div class="mnmfoto1"><a href="#"><img src="https://i.pinimg.com/564x/f0/cd/b6/f0cdb648aaf2b302373e41b4a904db08.jpg" alt="" width="250px"></a><p>Purple U</p><p>Rp. 50,000</p></div>
+                <div class="mnmfoto1"><a href="#"><img src="https://i.pinimg.com/564x/ed/d6/2b/edd62b96115c7263ed4a354ea022092b.jpg" alt="" width="250px"></a><p>Fresh Up</p><p>Rp. 50,000</p></div>
+                <div class="mnmfoto1"><a href="#"><img src="https://i.pinimg.com/564x/d6/8c/8f/d68c8fb6a70175d6e1afe0d8336f3532.jpg" alt="" width="250px"></a><p>Expert Purple</p><p>Rp. 50,000</p></div>
+                <div class="mnmfoto1"><a href="#"><img src="https://i.pinimg.com/564x/f6/82/fc/f682fcc37613e5f2c1337f0543f8fa2a.jpg" alt="" width="250px"></a><p>Basil</p><p>Rp. 50,000</p></div>
+                <div class="mnmfoto1"><a href="#"><img src="https://i.pinimg.com/564x/46/f5/f5/46f5f56729df37a81ebf55c7c88b32dd.jpg" alt="" width="250px"></a><p>Pick Me</p><p>Rp. 50,000</p></div>
+            </div>
+        </div>
+        <hr>
+
+        <div class="des">
+            <div class="des1"><h1>Dessert</h1></div>
+            <div class="des2"><h4>Rasakan lembutnya snow ice yang ditaburi dengan choco dan matcha powder dilengkapi dengan susu kental manis cokelat. <br> Berlaku hanya di Dine In.</h4></div>
+            <div class="desfoto">
+                <div class="desfoto1"><a href="#"><img src="https://i.pinimg.com/564x/98/ef/cd/98efcdf32070cd2d38f986d3e368b653.jpg" alt="" width="250px"></a><p>Cake Charm</p><p>Rp. 70,000</p></div>
+                <div class="desfoto1"><a href="#"><img src="https://i.pinimg.com/564x/dd/c8/42/ddc84210bbecc6322306ef37f51f4c25.jpg" alt="" width="250px"></a><p>Dashing Desserts</p><p>Rp. 70,000</p></div>
+                <div class="desfoto1"><a href="#"><img src="https://i.pinimg.com/564x/4a/cf/dd/4acfdd4722f19572c03feb0e1671c685.jpg" alt="" width="250px"></a><p>Sweet Craving</p><p>Rp. 85,000</p></div>
+                <div class="desfoto1"><a href="#"><img src="https://i.pinimg.com/564x/e4/3d/83/e43d83e9250464c95962ad6add31f735.jpg" alt="" width="250px"></a><p>Sweetopolis</p><p>Rp. 50,000</p></div>
+                <div class="desfoto1"><a href="#"><img src="https://i.pinimg.com/564x/62/0c/99/620c9918a45dac5dc0ca613af4a4c662.jpg" alt="" width="250px"></a><p>Delicate Confections Punch</p><p>Rp. 50,000</p></div>
+                <div class="desfoto1"><a href="#"><img src="https://i.pinimg.com/564x/36/2a/6a/362a6a4bbc8c4796621dad2de5e9f7c7.jpg" alt="" width="250px"></a><p>Dessert Land</p><p>Rp. 85,000</p></div>
+                <div class="desfoto1"><a href="#"><img src="https://i.pinimg.com/564x/32/4a/1a/324a1a6ab799bbdabdf6312fecb93939.jpg" alt="" width="250px"></a><p>Fantasy Cookies</p><p>Rp. 55,000</p></div>
+                <div class="desfoto1"><a href="#"><img src="https://i.pinimg.com/564x/65/d3/56/65d35692a76c5c47d81876c0a09cca8a.jpg" alt="" width="250px"></a><p>Sugar Crush</p><p>Rp. 65,000</p></div>
+                <div class="desfoto1"><a href="#"><img src="https://i.pinimg.com/564x/f7/0f/00/f70f0077bcd60dc90ef3208747cc4346.jpg" alt="" width="250px"></a><p>Take Desserts</p><p>Rp. 150,000</p></div>
+
+            </div>
+        </div>
+
+        <div class="menu"><a href="#">All Menu</a></div>
+
+        <div class="footer">
+            <footer>
+            <p>©Copyright 2022 - by Kania Medina Risma</p>
+            </footer>
+        </div>
+        
+
+    </div>
+
+    <script>
+        //start point
+        var i = 0;
+        var images = [];
+        var time = 3000;
+
+        //image list
+        images[0] = 'img/menu1.png';
+        images[1] = 'img/menu2.png';
+        images[2] = 'img/menu3.png';
+
+        //change image
+        function changeImg(){
+            document.slide.src = images[i];
+
+            if (i < images.length - 1){
+                i++;
+            } else{
+                i = 0;
+            }
+
+            setTimeout("changeImg()", time);
+        }
+
+        window.onload = changeImg;
+
+    </script>
+
+
+</body>
+</html>
