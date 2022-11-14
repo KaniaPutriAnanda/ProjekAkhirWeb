f13a3de5aab288468e9091ca18655388a614c2e7
KaniaPutriAnanda <93080099+KaniaPutriAnanda@users.noreply.github.com>
2022-11-14 20:08:13 +0800

    Update edit-mkn.php

--- edit-mkn.php
+++ edit-mkn.php
@@ -51,17 +51,88 @@ $row = mysqli_fetch_array($result);
         ?>
 
 
-<div>
-<form action="" method="POST"  enctype="multipart/form-data">
-            <label for="">Nama Makanan: </label> 
-            <input type="text" name="nama" required value = <?= $row['nama']?>> <br>
 
-            <label for="">Harga: </label> 
-            <input type="number" name="harga" required value = <?= $row['harga']?>> <br>
 
-            <label for="">Foto: </label><br><br>
-            <input type="file" name="gambar" required value = <?= $row['gambar']?>> <br><br>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/admin4.css?v8">
+    <!-- <style><?php include 'admin.css' ?></style> -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css" />
+    <link rel = "stylesheet" href = "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" />
+</head>
 
-            <button type="submit" name="submit">Submit</button>
+<body>
+<div class="bigboxbos">
+<nav>
+    <ul>
+        <li>
+            <a href="lihat.php" class="logo">
+                <i class="fas fa-search"></i>
+                <span class="nav-item">Lihat  Menu</span>
+            </a>
+        </li>
+        <li>
+            <a href="#">
+            <i class="fas fa-plus"></i>
+                <span class="nav-item">Tambah Menu</span>
+                <span class="fas fa-caret-down" id="panah"></span>
+            </a>
+                <ul class="feat-show">
+                    <li><a href="makanan.php">Makanan</a></li>
+                    <li><a href="minuman.php">Minuman</a></li>
+                    <li><a href="dessert.php">Dessert</a></li>
+                </ul>
+        </li>
+        <li>
+            <a href="lihat-edit.php">
+                <i class="fas fa-edit"></i>
+                <span class="nav-item">Update Menu</span>
+            </a>
+        </li>
+        <li>
+            <a href="lihat-hapus.php">
+                <i class="fas fa-trash"></i>
+                <span class="nav-item">Hapus Menu</span>
+            </a>
+        </li>
+        <li>
+            <a href="lihat_reservasi.php">
+                <i class="fas fa-cash-register"></i>
+                <span class="nav-item">Reservasi</span>
+            </a>
+        </li>
+        <li>
+            <a href="logout.php">
+                <i class="fas fa-sign-out-alt"></i>
+                <span class="nav-item">Signout</span>
+            </a>
+        </li>
+    </ul>
+</nav>
+<div class="bigboxForm">
+    <div class="kotakForm">
+            <form action="" method="POST"  enctype="multipart/form-data">
+                <div class="atasForm"><h1>Edit Menu Makanan </h1> <br><br></div>
+                    <label for="">Nama Makanan </label> 
+                    <input type="text" name="nama" required value = <?= $row['nama']?>>  <br>
 
+                    <label for="">Harga </label> 
+                    <input type="number" name="harga" required value = <?= $row['harga']?>> <br>
+
+                    <label for="">Foto </label>
+                    <input type="file" name="gambar" required value = <?= $row['gambar']?>><br>
+                
+                    <input type="hidden" name="tglinput" value=<?=date("d-m-Y")?>>
+                    <button type="submit" name="submit" id="button1" class="btn btn-primary deep-purple btn-block "><h1 class="submitt">Submit</h1></button>
+                
+                </form>
+        </div>
+    </div>
 </div>
+</body>
+</html>
